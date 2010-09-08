041f1895171fc418479ad4236b123d1e825f902a
Fabian Topfstedt <fabian@topfstedt.de>
2010-09-08 15:45:58 +0200

    first commit

---
+++ setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+
+
+setup(name='ez setup',
+      version='0.6c11',
+      description='Bootstrap setuptools installation',
+      url='http://peak.telecommunity.com/dist/ez_setup.py',
+     )
